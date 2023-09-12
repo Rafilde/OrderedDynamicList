ddf2ba3eaaa8d2659cc6d45ab07f08ce187781f1
Rafael Silva <rafasilva182003@gmail.com>
2023-09-12 06:37:59 -0300

    created my OrderedDynamicList

---
+++ src/List/Main.java
@@ -0,0 +1,12 @@
+package List;
+
+public class Main {
+	public static void main(String[] args) {
+		DrderedDynamicList teste = new DrderedDynamicList(); 
+		teste.add("3");
+		teste.add("1");
+		teste.add("5");
+		teste.add("4");
+		System.out.println(teste);
+	}
+}
